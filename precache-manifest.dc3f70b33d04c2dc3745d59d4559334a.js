95ccb60742e4442f2c7f24845053f2b6fef6bc3c
Ashley Trinh <ashley.trinh@hackbrightacademy.com>
2019-05-23 12:15:08 -0700

    deploy

---
+++ precache-manifest.dc3f70b33d04c2dc3745d59d4559334a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ed273da6adc11fc4dab1deb37c38ef",
+    "url": "/color-tool/index.html"
+  },
+  {
+    "revision": "ae531cf190aaeb9ab034",
+    "url": "/color-tool/static/css/main.a4406c32.chunk.css"
+  },
+  {
+    "revision": "2fecc3222d95f3199851",
+    "url": "/color-tool/static/js/2.f7ae880a.chunk.js"
+  },
+  {
+    "revision": "ae531cf190aaeb9ab034",
+    "url": "/color-tool/static/js/main.540cf8eb.chunk.js"
+  },
+  {
+    "revision": "431de8ad5b081a8efcbd",
+    "url": "/color-tool/static/js/runtime~main.54b4da09.js"
+  }
+]);
